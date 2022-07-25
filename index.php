d51ae68e1e6206065b8a461bb34c015cae44344d
babbaraniket <60863286+babbaraniket@users.noreply.github.com>
2022-07-25 11:36:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html> 
 	<head> 
-	<title>ANIKET-PHP</title>
+	<title>ANIKET BABBAR-PHP</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
-	<h1>PHP 2022  Test - Aniket Babbar</h1>
+	<h1>PHP 2022 Automation Test - Aniket Babbar</h1>
 		<p><b>Amdocs NA Session 4</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
